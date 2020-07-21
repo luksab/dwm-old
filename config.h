a2561b01db896256e05dc58bfbf43babc03a4ac1
luksab <luksablp@gmail.com>
2020-07-21 23:00:17 +0200

    tweaks...

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
-static const unsigned int snap = 32;	/* snap pixel */
+static const unsigned int borderpx = 0; /* border pixel of windows */
+static const unsigned int snap = 8;		/* snap pixel */
 static const int showbar = 1;			/* 0 means no bar */
 static const int topbar = 1;			/* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
@@ -13,11 +13,11 @@ static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+static const char col_bg[] = "#222222";
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-	[SchemeSel] = {col_gray4, col_cyan, col_cyan},
+	[SchemeSel] = {col_gray4, col_bg, col_bg},
 };
 
 /* tagging */
@@ -31,6 +31,9 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{"Gimp", NULL, NULL, 0, 1, -1},
 	{"Firefox", NULL, NULL, 1 << 8, 0, -1},
+	{"xwinwrap", NULL, NULL, 1 << 9, False, -1},
+	{"mplayer", NULL, NULL, 1 << 9, False, -1},
+	{"mpv", NULL, NULL, 1 << 9, False, -1},
 };
 
 /* layout(s) */
@@ -61,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
+static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_bg, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 
 // pulse volume
@@ -81,6 +84,8 @@ static const char *randomBG[] = {"feh", "--bg-center", "--randomize", randomBGDi
 // screen brightness
 static const char *brightness_up[] = {"xbacklight", "+10", NULL};
 static const char *brightness_down[] = {"xbacklight", "-10", NULL};
+static const char *brightness_up_slight[] = {"xbacklight", "+1", NULL};
+static const char *brightness_down_slight[] = {"xbacklight", "-1", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -125,6 +130,8 @@ static Key keys[] = {
 	//screen brightness
 	{0, XF86XK_MonBrightnessUp, spawn, {.v = brightness_up}},
 	{0, XF86XK_MonBrightnessDown, spawn, {.v = brightness_down}},
+	{ShiftMask, XF86XK_MonBrightnessUp, spawn, {.v = brightness_up_slight}},
+	{ShiftMask, XF86XK_MonBrightnessDown, spawn, {.v = brightness_down_slight}},
 
 	//spotifyd control
 	{0, XF86XK_AudioPlay, spawn, {.v = SpotifyPlayPause}},
